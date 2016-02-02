eb5381110ed8031a22e8b6e1d3b2ca0cee2f8dc2
R. Alex Clark <aclark2089@gmail.com>
2016-02-02 18:59:41 -0500

    Setup Tweeter

---
+++ Tweeter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tweeter
+//
+//  Created by Alex Clark on 2/2/16.
+//  Copyright © 2016 R. Alex Clark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
